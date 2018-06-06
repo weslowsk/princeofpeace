d7ae17d9f95c68c88418d978fada9388b3a68e34
Kevin Weslowski <kevin.weslowski@gmail.com>
2018-06-05 19:44:04 -0600

    added functionality to contact form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,22 @@
-function emailForm() {
-    
+function handleEmailFormSubmit(form, baseAction, emailAddress) {
+    var newAction = baseAction.concat(emailAddress);
+    form.action = newAction;
+    form.submit();
+}
+
+function handleEmailFormOnLoadEvent() {
+    var formSubmitted = getParameterByName('formSubmitted');
+    if (formSubmitted) {
+        alert("Your message was sent!");
+    }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
